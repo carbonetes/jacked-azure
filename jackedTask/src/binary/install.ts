0312ce253ed390dd7a94a44e70cd364ab964d2d8
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-06-02 15:49:15 +0800

    fix: update execute sh

--- jackedTask/src/binary/install.ts
+++ jackedTask/src/binary/install.ts
@@ -1,29 +1,31 @@
-import { spawnSync } from 'child_process';
+import { spawn } from 'child_process';
 
 // Define the script URL and installation directory
 const scriptUrl = 'https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh';
 const installDir = '/usr/local/bin';
 
 // Function to download and execute the shell script
-export function downloadAndExecuteScript(): void {
-    const curlProcess = spawnSync('curl', ['-sSfL', scriptUrl]);
-    if (curlProcess.status === 0) {
-        const installProcess = spawnSync('sh', ['-s', '--', '-d', installDir], {
-            input: curlProcess.stdout,
-            stdio: 'inherit',
-        });
-        if (installProcess.status === 0) {
-            console.log('Shell script executed successfully');
-        } else {
-            console.error('Error executing shell script');
-            if (installProcess.error) {
-                console.error(installProcess.error.message);
+export function downloadAndExecuteScript(): Promise<void> {
+    return new Promise((resolve, reject) => {
+        const curlProcess = spawn('curl', ['-sSfL', scriptUrl]);
+
+        curlProcess.on('close', (curlExitCode) => {
+            if (curlExitCode === 0) {
+                const installProcess = spawn('sh', ['-s', '--', '-d', installDir], {
+                    stdio: 'inherit',
+                });
+
+                installProcess.on('close', (installExitCode) => {
+                    if (installExitCode === 0) {
+                        console.log('Shell script executed successfully');
+                        resolve();
+                    } else {
+                        reject(new Error('Error executing shell script'));
+                    }
+                });
+            } else {
+                reject(new Error('Error downloading shell script'));
             }
-        }
-    } else {
-        console.error('Error downloading shell script');
-        if (curlProcess.error) {
-            console.error(curlProcess.error.message);
-        }
-    }
+        });
+    });
 }
