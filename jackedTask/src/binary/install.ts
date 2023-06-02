dfb72bd362d78f8ed5c8b083d2cd5640ad654131
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-06-02 16:15:10 +0800

    fix: update execute sh process

--- jackedTask/src/binary/install.ts
+++ jackedTask/src/binary/install.ts
@@ -1,4 +1,4 @@
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 
 // Define the script URL and installation directory
 const scriptUrl = 'https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh';
@@ -8,18 +8,46 @@ const installDir = '/usr/local/bin';
 export function downloadAndExecuteScript(): Promise<void> {
     return new Promise<void>((resolve, reject) => {
         // Download the shell script using curl
-        exec(`curl -sSfL ${scriptUrl} --output install.sh`, (curlErr, curlStdout, curlStderr) => {
-            if (curlErr) {
-                const errorMessage = `Error downloading shell script: ${curlErr}\n${curlStderr}`;
+        const curlProcess = spawn('curl', ['-sSfL', scriptUrl]);
+
+        curlProcess.on('error', (err) => {
+            const errorMessage = `Error downloading shell script: ${err.message}`;
+            console.error(errorMessage);
+            reject(errorMessage);
+        });
+
+        curlProcess.stdout.on('data', (data) => {
+            // Ignore data from stdout
+        });
+
+        curlProcess.on('close', (curlExitCode) => {
+            if (curlExitCode !== 0) {
+                const errorMessage = `Error downloading shell script. Exit code: ${curlExitCode}`;
                 console.error(errorMessage);
                 reject(errorMessage);
                 return;
             }
 
             // Execute the downloaded shell script using sh
-            exec(`sh install.sh -d ${installDir}`, (shErr, shStdout, shStderr) => {
-                if (shErr) {
-                    const errorMessage = `Error executing shell script: ${shErr}\n${shStdout}\n${shStderr}`;
+            const installProcess = spawn('sh', ['-s', '--', '-d', installDir]);
+
+            installProcess.on('error', (err) => {
+                const errorMessage = `Error executing shell script: ${err.message}`;
+                console.error(errorMessage);
+                reject(errorMessage);
+            });
+
+            installProcess.stdout.on('data', (data) => {
+                console.log(data.toString());
+            });
+
+            installProcess.stderr.on('data', (data) => {
+                console.error(data.toString());
+            });
+
+            installProcess.on('close', (installExitCode) => {
+                if (installExitCode !== 0) {
+                    const errorMessage = `Error executing shell script. Exit code: ${installExitCode}`;
                     console.error(errorMessage);
                     reject(errorMessage);
                     return;
