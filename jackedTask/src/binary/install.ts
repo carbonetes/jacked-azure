18d1ab2e6c928f3e48d43a20fd4a13d904e369b4
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-06-02 16:02:49 +0800

    fix: update execute process

--- jackedTask/src/binary/install.ts
+++ jackedTask/src/binary/install.ts
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 
 // Define the script URL and installation directory
 const scriptUrl = 'https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh';
@@ -6,33 +6,26 @@ const installDir = '/usr/local/bin';
 
 // Function to download and execute the shell script
 export function downloadAndExecuteScript(): Promise<void> {
-    return new Promise((resolve, reject) => {
-        const curlProcess = spawn('curl', ['-sSfL', scriptUrl]);
-
-        let curlOutput = '';
-        curlProcess.stdout.on('data', (data) => {
-            curlOutput += data;
-        });
-
-        curlProcess.on('close', (curlExitCode) => {
-            if (curlExitCode === 0) {
-                const installProcess = spawn('sh', ['-s', '--', '-d', installDir]);
-
-                installProcess.on('close', (installExitCode) => {
-                    if (installExitCode === 0) {
-                        console.log('Shell script executed successfully');
-                        resolve();
-                    } else {
-                        reject(new Error(`Error executing shell script: ${curlOutput}`));
-                    }
-                });
-
-                // Forward the stdout and stderr of the install process to the task console
-                installProcess.stdout.pipe(process.stdout);
-                installProcess.stderr.pipe(process.stderr);
-            } else {
-                reject(new Error(`Error downloading shell script: ${curlOutput}`));
+    return new Promise<void>((resolve, reject) => {
+        // Download the shell script using curl
+        exec(`curl -sSfL ${scriptUrl} --output install.sh`, (curlErr) => {
+            if (curlErr) {
+                console.error('Error downloading shell script:', curlErr);
+                reject(curlErr);
+                return;
             }
+
+            // Execute the downloaded shell script using sh
+            exec(`sh install.sh -d ${installDir}`, (shErr) => {
+                if (shErr) {
+                    console.error('Error executing shell script:', shErr);
+                    reject(shErr);
+                    return;
+                }
+
+                console.log('Shell script executed successfully');
+                resolve();
+            });
         });
     });
 }
