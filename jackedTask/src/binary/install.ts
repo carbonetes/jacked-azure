931b5cc00316dc82e7e3bf1eeb6eced0ad83ac60
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-06-02 15:55:46 +0800

    fix: executing sh

--- jackedTask/src/binary/install.ts
+++ jackedTask/src/binary/install.ts
@@ -9,22 +9,29 @@ export function downloadAndExecuteScript(): Promise<void> {
     return new Promise((resolve, reject) => {
         const curlProcess = spawn('curl', ['-sSfL', scriptUrl]);
 
+        let curlOutput = '';
+        curlProcess.stdout.on('data', (data) => {
+            curlOutput += data;
+        });
+
         curlProcess.on('close', (curlExitCode) => {
             if (curlExitCode === 0) {
-                const installProcess = spawn('sh', ['-s', '--', '-d', installDir], {
-                    stdio: 'inherit',
-                });
+                const installProcess = spawn('sh', ['-s', '--', '-d', installDir]);
 
                 installProcess.on('close', (installExitCode) => {
                     if (installExitCode === 0) {
                         console.log('Shell script executed successfully');
                         resolve();
                     } else {
-                        reject(new Error('Error executing shell script'));
+                        reject(new Error(`Error executing shell script: ${curlOutput}`));
                     }
                 });
+
+                // Forward the stdout and stderr of the install process to the task console
+                installProcess.stdout.pipe(process.stdout);
+                installProcess.stderr.pipe(process.stderr);
             } else {
-                reject(new Error('Error downloading shell script'));
+                reject(new Error(`Error downloading shell script: ${curlOutput}`));
             }
         });
     });
