65e44e9f7873a262feda8fc8f66ff3dabdb1aa63
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-06-05 11:17:02 +0800

    fix: update execute process

--- jackedTask/src/binary/execute.ts
+++ jackedTask/src/binary/execute.ts
@@ -2,10 +2,11 @@ import { spawnSync } from 'child_process';
 
 // Function to execute the 'jacked' command
 export function executeJackedCommand(command: string, successMessage: string, failureMessage: string): void {
-    const jackedProcess = spawnSync(command, { stdio: 'inherit', shell: true });
+    const jackedProcess = spawnSync(command, { shell: true });
+
     if (jackedProcess.status === 0) {
         console.log(successMessage);
     } else {
-        console.error(failureMessage);
+        console.error(`${failureMessage}: ${jackedProcess.stderr}`);
     }
 }
