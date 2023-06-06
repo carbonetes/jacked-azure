b50f6e2eed78cf96d3f208e7ae85d24b9f14a7e8
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-06-06 15:44:47 +0800

    fix: use home dir

--- jackedTask/src/binary/install.ts
+++ jackedTask/src/binary/install.ts
@@ -1,27 +1,23 @@
 import { exec } from 'child_process';
+import { homedir } from 'os';
 
 function executeScript(): Promise<void> {
     return new Promise<void>((resolve, reject) => {
-        const command = 'sudo curl -sSfL https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh | sh -s -- -d /usr/bin';
+        const homeDir = homedir();
+        const command = `curl -sSfL https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh | sh -s -- -d ${homeDir}`;
         const installProcess = exec(command);
 
         installProcess.stderr?.on('data', (data) => {
-            console.error(data.toString()); 
+            console.error(data.toString());
         });
 
         installProcess.on('exit', (code, signal) => {
             if (code === 0) {
                 console.log('Script executed successfully');
-                if (installProcess.stdout) {
-                    console.log('Script output:', installProcess.stdout.toString());
-                }
                 resolve();
             } else {
                 const errorMessage = `Error executing script. Exit code: ${code}, Signal: ${signal}`;
                 console.error(errorMessage);
-                if (installProcess.stdout) {
-                    console.log('Script output:', installProcess.stdout.toString());
-                }
                 reject(errorMessage);
             }
         });
