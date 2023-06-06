1b30169720ca43c2d0ff0ad4713a02c66454434b
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-06-06 10:06:28 +0800

    fix: update setup homebrew on unix

--- jackedTask/src/homebrew/install.ts
+++ jackedTask/src/homebrew/install.ts
@@ -1,32 +1,28 @@
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 
-export function installPackage(packageName: string): Promise<void> {
+export function installHomebrew(): Promise<void> {
     return new Promise<void>((resolve, reject) => {
-        const installProcess = spawn('apt', ['install', '-y', packageName]);
+        const installCommand = 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"';
 
-        installProcess.on('error', (err) => {
-            const errorMessage = `Error installing package: ${err.message}`;
-            console.error(errorMessage);
-            reject(errorMessage);
-        });
+        const installProcess = exec(installCommand);
 
-        installProcess.stdout.on('data', (data) => {
-            console.log(data.toString());
-        });
-
-        installProcess.stderr.on('data', (data) => {
+        installProcess.stderr?.on('data', (data) => {
             console.error(data.toString());
         });
 
-        installProcess.on('close', (exitCode) => {
-            if (exitCode !== 0) {
-                const errorMessage = `Error installing package. Exit code: ${exitCode}`;
+        installProcess.on('exit', (code) => {
+            if (code === 0) {
+                console.log('Homebrew installed successfully');
+                resolve();
+            } else {
+                const errorMessage = `Error installing Homebrew. Exit code: ${code}`;
                 console.error(errorMessage);
                 reject(errorMessage);
-            } else {
-                console.log(`Package ${packageName} installed successfully`);
-                resolve();
             }
         });
     });
 }
+
+installHomebrew().catch((error) => {
+    console.error('Error installing Homebrew:', error);
+});
