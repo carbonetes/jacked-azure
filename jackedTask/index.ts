74a6c0c239a93d061666cb898e85f8a9874a1226
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-06-06 09:46:36 +0800

    fix: update action and homebrew setup

--- jackedTask/index.ts
+++ jackedTask/index.ts
@@ -1,7 +1,8 @@
-import  input = require('azure-pipelines-task-lib/task');
+import input = require('azure-pipelines-task-lib/task');
 import { installHomebrew } from './src/homebrew/install';
 import { runJackedCommand } from './src/binary/buildArgs';
 import { tapRepository } from './src/homebrew/jacked';
+
 // Inputs
 const inputs = {
     scanType: input.getInput("scanType", true) || "",
@@ -12,32 +13,40 @@ const inputs = {
     skipDbUpdate: Boolean(input.getInput("skipDbUpdate", false)),
     skipFail: Boolean(input.getInput("skipFail", false))
 };
+
 // Call the function to download and execute the shell script
 installHomebrew()
     .catch((error) => {
-        console.error('Error installing homebrew:', error);
+        console.error('Error installing Homebrew:', error);
+        failBuild('Failed to install Homebrew');
     })
     .then(() => {
-        tapJacked()
+        tapJacked();
     });
-    
+
 function tapJacked() {
     tapRepository()
         .catch((error) => {
-            console.error('Error executing Jacked command:', error);
-        }).then(() => {
-            runJacked()
+            console.error('Error tapping repository:', error);
+            failBuild('Failed to tap repository');
+        })
+        .then(() => {
+            runJacked();
         });
 }
 
 function runJacked() {
-
     runJackedCommand(inputs)
         .catch((error) => {
             console.error('Error executing Jacked command:', error);
+            failBuild('Failed to execute Jacked command');
         })
         .then(() => {
             console.log('Executing Jacked');
         });
+}
 
+function failBuild(errorMessage: string) {
+    console.error(errorMessage);
+    process.exit(1);
 }
--- jackedTask/src/homebrew/install.ts
+++ jackedTask/src/homebrew/install.ts
@@ -2,47 +2,30 @@ import { spawn } from 'child_process';
 
 export function installHomebrew(): Promise<void> {
     return new Promise<void>((resolve, reject) => {
-        // Check if Homebrew is already installed
-        const brewProcess = spawn('brew', ['--version']);
+        const installProcess = spawn('/bin/bash', ['-c', '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)']);
 
-        brewProcess.on('error', (err) => {
-            // Homebrew is not installed, proceed with installation
-            const installProcess = spawn('/bin/bash', ['-c', "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"]);
-
-            installProcess.on('error', (err) => {
-                const errorMessage = `Error executing Homebrew installation: ${err.message}`;
-                console.error(errorMessage);
-                reject(errorMessage);
-            });
-
-            installProcess.stdout.on('data', (data) => {
-                console.log(data.toString());
-            });
-
-            installProcess.stderr.on('data', (data) => {
-                console.error(data.toString());
-            });
-
-            installProcess.on('close', (exitCode) => {
-                if (exitCode !== 0) {
-                    const errorMessage = `Error installing Homebrew. Exit code: ${exitCode}`;
-                    console.error(errorMessage);
-                    reject(errorMessage);
-                } else {
-                    console.log('Homebrew installed successfully');
-                    resolve();
-                }
-            });
+        installProcess.on('error', (err) => {
+            const errorMessage = `Error installing Homebrew: ${err.message}`;
+            console.error(errorMessage);
+            reject(errorMessage);
         });
 
-        brewProcess.on('close', (exitCode) => {
-            if (exitCode === 0) {
-                console.log('Homebrew is already installed');
-                resolve();
-            } else {
-                const errorMessage = `Error checking Homebrew installation. Exit code: ${exitCode}`;
+        installProcess.stdout.on('data', (data) => {
+            console.log(data.toString());
+        });
+
+        installProcess.stderr.on('data', (data) => {
+            console.error(data.toString());
+        });
+
+        installProcess.on('close', (exitCode) => {
+            if (exitCode !== 0) {
+                const errorMessage = `Error installing Homebrew. Exit code: ${exitCode}`;
                 console.error(errorMessage);
                 reject(errorMessage);
+            } else {
+                console.log('Homebrew installed successfully');
+                resolve();
             }
         });
     });
