535432fd0ef958ecf9febb03cacf77c27a1a4602
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-06-09 13:12:10 +0800

    fix: update custom error for fail build

--- jackedTask/src/binary/execute.ts
+++ jackedTask/src/binary/execute.ts
@@ -2,17 +2,20 @@ import { exec, ExecOptions } from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
 import { homedir } from 'os';
-import { exit } from "process";
+import { exit } from 'process';
 
-
-export function executeCommand(command: string, failedSeverity: string, failureMessage: string): void {
+export function executeCommand(
+    command: string,
+    failedSeverity: string,
+    failureMessage: string
+): void {
     const homeDir = homedir();
     const jackedBinaryPath = path.join(homeDir, 'jacked');
 
     // Check if the 'jacked' binary file exists
     if (!fs.existsSync(jackedBinaryPath) || !fs.lstatSync(jackedBinaryPath).isFile()) {
         console.error(`${failureMessage}: 'jacked' binary not found`);
-        return;
+        exit(1);
     }
 
     // Check the permissions of the 'jacked' binary
@@ -25,55 +28,33 @@ export function executeCommand(command: string, failedSeverity: string, failureM
         console.log(`Executable permission set for 'jacked' binary`);
     }
 
-
-    const execOptions = {
+    const execOptions: ExecOptions = {
         cwd: '.',
         maxBuffer: 1024 * 1024 * 250, // Set a higher value for maxBuffer (e.g., 250MB)
         shell: '/bin/bash',
-        stdio: ['pipe', 'pipe', 'ignore'] // Redirect error output to /dev/null
     };
 
-    exec(`${jackedBinaryPath} ${command}`, execOptions, (error, stdout, stderr) => {
-        if (error && error.code !== 0) {
-            console.error(`Error running 'jacked' command: ${error.message}`);
-            process.exit(1); // Exit the process with a non-zero status code to indicate failure
+    const childProcess = exec(`${jackedBinaryPath} ${command}`, execOptions);
+    childProcess.stdout?.on('data', (data) => {
+        const log = data.toString().trim();
+        console.log(log);
+    });
+
+    childProcess.stderr?.on('data', (data) => {
+        // Ignore stderr output
+    });
+
+    childProcess.on('error', (error) => {
+        console.error(`Error running 'jacked' command: ${error.message}`);
+        exit(1);
+    });
+
+    childProcess.on('exit', (code) => {
+        if (code === 0) {
+            exit(0);
+        } else {
+            console.log(`Jacked assessment has failed. Please see recommendations to fix vulnerabilities.`);
+            exit(1);
         }
-
-        const logBuffer = [];
-
-        // Capture stdout in a log buffer
-        if (stdout) {
-            logBuffer.push(stdout);
-        }
-
-        // Filter and trim the logs
-        const logs = logBuffer.join('').split('\n')
-            .filter((log) => !log.includes('Extracting files'))
-            .filter((log) => !log.includes('Downloading updated database'))
-            .map((log) => log.trim())
-            .filter((log) => log !== '');
-
-        // Check for failure in logs
-        let failureMessage = '';
-        for (const log of logs) {
-            if (log.includes('Failed:')) {
-                failureMessage = log;
-                break;
-            }
-        }
-
-
-        // Print the logs in the correct order
-        for (const log of logs) {
-            console.log(log);
-        }
-
-        if (failureMessage) {
-            console.log(`Jacked assessment is failed. Please see recommendation to fix vulnerabilities`);
-            process.exitCode = 1; // Set the process exit code to 1 to indicate failure
-            return;
-        }
-
-        process.exitCode = 0; // Set the process exit code to 0 to indicate success
     });
 }
